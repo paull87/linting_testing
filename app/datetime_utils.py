14fa7ab4b347fae8ac13877c8efc3a093928af0f
Paul Lucas <plucas@hometrack.com>
2020-03-09 16:13:14 +0000

    Try to commit

--- app/datetime_utils.py
+++ app/datetime_utils.py
@@ -4,3 +4,7 @@ import datetime
 def get_today():
     """..............................................................."""
     return datetime.datetime.now()
+
+
+def get_today_string():
+    return get_today().strftime("%Y%M%d")
